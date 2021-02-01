cd54b197514f5109e1cd1ebed051a5fee6cf7cb4
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2021-02-01 09:11:43 +0530

    adding initial pages

---
+++ lib/SignInPages/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Login Page")),
+    );
+  }
+}
